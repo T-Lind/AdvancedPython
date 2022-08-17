8807b625dc0acd30e147895455e1455ce41aa10d
T-Lind <tiernanxklindauer@gmail.com>
2022-08-17 15:31:28 -0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+mylist = [1, 9, 3, 0, -4]
+
+# Print the second to last item
+print(mylist[-2])
+
+if 0 in mylist:
+    print("yes")
+else:
+    print("no")
+
+# Reversed list
+reversedlist = mylist.reverse()
+print("Reversed list",mylist)
